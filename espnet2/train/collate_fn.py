787b02bdc1134c4a997fcb38ff24a32f686ee5e0
hiranoyu0830 <yuta.hirano.0830@gmail.com>
2024-10-23 12:56:13 +0900

    Add a new key 'num_spk' for EDA loss calculation

--- espnet2/train/collate_fn.py
+++ espnet2/train/collate_fn.py
@@ -224,24 +224,28 @@ def common_collate_fn(
         # Adjust the number of spkrs to the maximum number of spkrs in the mini-batch
         if key == "spk_labels":
             max_spk = 4 # You can change this value as you like
-            
+            new_key = "num_spk"
+
             # deal with one by one in the mini-batch
             for d_spk in data:
                 # d_spk[key]: (num_spkrs, num_frames)
                 num_speakers = len(d_spk[key][0])
-                assert num_speakers <= 10, f"num_speakers: {num_speakers} > 10"
+                assert num_speakers <= 10, f"too large num_speakers: {num_speakers} > 10"
                 if num_speakers < max_spk:
                     # create inactive speakers by zero padding
                     d_spk[key] = np.pad(d_spk[key], ((0, 0), (0, max_spk - num_speakers)), mode='constant')
+                    d_spk[new_key] = num_speakers
                 elif num_speakers > max_spk:
                     # sort speakers in descending talkativeness order
                     indices = np.argsort(-np.sum(d_spk[key], axis=0), axis=0)
                     # keep the most talkative speakers
                     d_spk[key][-1] = d_spk[key][indices[:max_spk]]
+                    d_spk[new_key] = max_spk
                 else:
-                    pass
+                    d_spk[new_key] = num_speakers
                 assert len(d_spk[key][0]) == max_spk, f"num_spkrs: {len(d_spk[key].shape[0])}, not equal to max_spk: {max_spk}. The shape of spk_labels is {d_spk[key].shape}"
-
+        
+        # this operation is also applied to the 'num_spk' key
         array_list = [d[key] for d in data]
 
         # Assume the first axis is length:
